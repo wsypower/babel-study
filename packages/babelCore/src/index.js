4e27e3c69a20bedc987d07b793462fcf43099877
weiyafei <2553241022@qq.com>
2023-02-09 15:37:23 +0800

    feat: transformFileSync

--- packages/babelCore/src/index.js
+++ packages/babelCore/src/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-02-08 14:38:23
- * @LastEditTime: 2023-02-08 15:19:03
+ * @LastEditTime: 2023-02-09 15:10:45
  * @LastEditors: wsy
  */
 import core from '@babel/core'
@@ -11,8 +11,21 @@ const require = createRequire(import.meta.url)
 const code = require.resolve('./origin.js')
 
 const ast = core.transformFileSync(code, {
-  plugins: [],
-  presets: ["@babel/preset-env"]
+  // plugins: [
+  //   [
+  //     "@babel/plugin-transform-runtime", {
+  //       "corejs": "3",
+  //     }
+  //   ],
+  // ],
+  presets: [
+    ["@babel/preset-env", {
+      // "browserslistEnv": "dd",
+      "useBuiltIns": "usage",
+      "corejs": "3.22",
+      "debug": true
+    }]
+  ]
 });
 
 console.log(ast.code)
--- packages/babelCore/src/origin.js
+++ packages/babelCore/src/origin.js
@@ -1,2 +1,2 @@
-const a = [1, 2, 3];
-a.at(-1);
+const a = [];
+a.fill(-1);
