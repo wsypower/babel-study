37f132e41ec300214cb17646dfeaafd52bafac55
weiyafei <2553241022@qq.com>
2023-04-10 14:08:22 +0800

    stop

--- packages/book-test/stop.js
+++ packages/book-test/stop.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: wsy
  * @Date: 2023-04-06 14:52:53
- * @LastEditTime: 2023-04-07 18:47:58
+ * @LastEditTime: 2023-04-10 08:30:49
  * @LastEditors: wsy
  */
 const { transform, types: t } = require('@babel/core')
@@ -19,11 +19,10 @@ transform(code, {
     {
       visitor: {
         FunctionDeclaration(path) {
-
+          path.context.foo = 'bar'
         },
-        Identifier(path) {
-          console.log(`"${path.node.name}"`);
-          path.stop()
+        Identifier(path,) {
+          console.log(`"${path.node.name}",--------`, path.context.foo);
         }
       }
     }
