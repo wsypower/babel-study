52d97260bef8383dab202851362f9c618685fb44
weiyafei <2553241022@qq.com>
2023-04-12 15:58:00 +0800

    getNextSibling

---
+++ packages/book-test/getPrevSibling.js
@@ -0,0 +1,46 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-04-06 14:52:53
+ * @LastEditTime: 2023-04-12 15:52:16
+ * @LastEditors: wsy
+ */
+const { transform, types: t } = require('@babel/core')
+
+const code = `
+function foo() {
+  const a = 3
+  const b = 1
+  const c = 1
+  const e = 1
+}
+`;
+
+
+transform(code, {
+  plugins: [
+    {
+      visitor: {
+        VariableDeclaration(path, state) {
+          if (path.key > 1) {
+            console.log(path.key)
+            console.log(path.node.declarations[0].id.name)
+            console.log(path.getNextSibling().node.declarations[0].id.name)
+            path.stop()
+          }
+
+        }
+      }
+    }
+  ]
+},
+  (err, result) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('\n\r')
+      console.log('------- code -------')
+      console.log('\n\r')
+      console.log(result.code);
+    }
+  })
